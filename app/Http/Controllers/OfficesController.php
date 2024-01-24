fbf81e4e670ccb6c732247517d1bed8136f765c7
ravilarcdev <ravil9423@outlook.com>
2024-01-24 20:52:38 +0800

    update the offices page

--- app/Http/Controllers/OfficesController.php
+++ app/Http/Controllers/OfficesController.php
@@ -8,13 +8,17 @@ use App\Office;
 class OfficesController extends Controller
 {
     public function index() {
-        return view ('pages.offices.index');
+        $offices = Office::get()->groupBy(function($data) {
+            return $data->country;
+        });
+        return view ('pages.offices.index')
+            -> with('offices', $offices);
     }
 
     public function officeFilter(Request $request) {
-        $country = $request->get('country_name');
-        if ($country) {
-            $offices = Office::query()->where('country', 'LIKE', "%{$country}%")->get();
+        $id = $request->get('office_id');
+        if ($id) {
+            $offices = Office::where('id', '=', $id)->get();
             return response()->json($offices);
         }
     }
