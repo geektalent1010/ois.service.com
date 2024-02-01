e5da06e34b519efceab180dfe86cf15c5d5dbeef
Full Stack Engineer <geektalent1010@gmail.com>
2024-02-01 02:57:24 -0800

    UPDATE: landing page for new design

--- resources/views/_includes/navbar.blade.php
+++ resources/views/_includes/navbar.blade.php
@@ -4,49 +4,49 @@
             <div class="navbarItem">
                 @guest
                     <a class="navbar-brand pl-1" href="{{ route('landing') }}">
-                @else
-                    <a class="navbar-brand pl-1" href="{{ route('dashboard.index') }}">
-                @endguest
+                    @else
+                        <a class="navbar-brand pl-1" href="{{ route('dashboard.index') }}">
+                        @endguest
                         <img src="{{ asset('images/Logo/LogoOIS1.svg') }}" class="img-fluid logo-ois" />
                     </a>
+                    <a class="locate-button" id="locate-button">EN</a>
             </div>
             <div class="navbarItem text-center">
                 @if (isset($ACTIVE_TITLE))
-                <span class="navbar-title">
-                    {{ $ACTIVE_TITLE }}
-                </span>
+                    <span class="navbar-title">
+                        {{ $ACTIVE_TITLE }}
+                    </span>
                 @endif
             </div>
             <div class="navbarItem d-flex justify-content-end">
                 @guest
-                    <a href="{{ route('dashboard.index') }}" class="d-flex align-items-center">
+                    <a id="menu-item" class="d-flex align-items-center navbar-menu-item">
+                        MENU
                         <img class="menu-icon" src="{{ asset('images/IconMENU.svg') }}" alt="" />
-                    </a>
-                    <a onclick="toggleChatbotModal()" class="d-flex align-items-center">
-                        <img class="chatbot-icon" src="{{ asset('images/Logo/IconCHATBOT1.svg') }}" alt="" />
                     </a>
                     @if (!isset($ACTIVE_LOGOUT))
-                    <a href="{{ route('login') }}" class="d-flex align-items-center">
-                        <img class="login-icon" src="{{ asset('images/IconLOGIN.svg') }}" alt="" />
-                    </a>
+                        <a href="{{ route('login') }}" class="d-flex align-items-center navbar-login-item">
+                            LOGIN
+                            <img class="login-icon" src="{{ asset('images/IconLOGIN.svg') }}" alt="" />
+                        </a>
                     @else
-                    <a href="{{ route('logout') }}" class="d-flex align-items-center"
-                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                        <img src="{{ asset('images/IconLOGOUT.svg') }}" alt="Logout" class="logout" />
-                    </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                        @csrf
-                    </form>
+                        <a href="{{ route('logout') }}" class="d-flex align-items-center navbar-logout-item"
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            LOGOUT
+                            <img src="{{ asset('images/IconLOGOUT.svg') }}" alt="Logout" class="logout" />
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
                     @endif
                 @else
-                    <a href="{{ route('dashboard.index') }}" class="d-flex align-items-center">
+                    <a id="menu-item" class="d-flex align-items-center navbar-menu-item">
+                        MENU
                         <img class="menu-icon" src="{{ asset('images/IconMENU.svg') }}" alt="" />
                     </a>
-                    <a onclick="toggleChatbotModal()" class="d-flex align-items-center">
-                        <img class="chatbot-icon" src="{{ asset('images/Logo/IconCHATBOT1.svg') }}" alt="" />
-                    </a>
-                    <a href="{{ route('logout') }}" class="d-flex align-items-center"
+                    <a href="{{ route('logout') }}" class="d-flex align-items-center navbar-logout-item"
                         onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        LOGOUT
                         <img src="{{ asset('images/IconLOGOUT.svg') }}" alt="Logout" class="logout" />
                     </a>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
@@ -56,5 +56,102 @@
             </div>
         </div>
     </nav>
+    <div class="left-menubar" id="left-menubar">
+        <div class="lang-section" id="menu-list">
+            <div class="lang-item"><a class="item">CHINESE</a></div>
+            <div class="lang-item"><a class="item active">ENGLISH</a></div>
+            <div class="lang-item"><a class="item">FRENCH</a></div>
+            <div class="lang-item"><a class="item">GERMAN</a></div>
+            <div class="lang-item"><a class="item">PORTUGUESE</a></div>
+            <div class="lang-item"><a class="item">SPANISH</a></div>
+        </div>
+    </div>
+
+    <div class="right-menubar" id="right-menubar">
+        <div class="lang-section">
+            <div class="lang-item"><a class="item">ABOUT OIS</a></div>
+            <div class="lang-item"><a class="item active">SERVICES</a></div>
+            <div class="lang-item"><a class="item">INNOVATION</a></div>
+            <div class="lang-item"><a class="item">SECURITY</a></div>
+        </div>
+        <div class="lang-section">
+            <div class="lang-item"><a class="item">VISA APPLICATION</a></div>
+            <div class="lang-item"><a class="item">PASSPORT APPLICATION</a></div>
+            <div class="lang-item"><a class="item">BVN APPLICATION</a></div>
+            <div class="lang-item"><a class="item">TRACK VISA APPLICATION</a></div>
+        </div>
+        <div class="lang-section">
+            <div class="lang-item"><a class="item">E-HEALTH PORTAL</a></div>
+            <div class="lang-item"><a class="item">MONEY PORTAL</a></div>
+            <div class="lang-item"><a class="item">SHOPPING PORTAL</a></div>
+        </div>
+        <div class="lang-section">
+            <div class="lang-item"><a class="item">CUSTOMER SUPPORT</a></div>
+        </div>
+        <div class="lang-section">
+            <div class="lang-item"><a class="item">OIS GLOBAL CETERS</a></div>
+        </div>
+    </div>
+    <div class="dark-bg" id="dark-bg"></div>
 </div>
 
+<script>
+    // ------------------start left navbar script(author:YU)----------------------
+    const leftMenuBar = document.getElementById('left-menubar');
+    const locateButton = document.getElementById('locate-button');
+    const menuList = document.getElementById('menu-list');
+    const menuItems = leftMenuBar.getElementsByClassName('item');
+    const darkBg = document.getElementById('dark-bg');
+    locateButton.addEventListener('mouseover', () => {
+        leftMenuBar.classList.add('open');
+        darkBg.classList.add('block');
+    });
+
+    menuList.addEventListener('click', (event) => {
+        const clickedMenuItem = event.target;
+        if (clickedMenuItem.classList.contains('item')) {
+            Array.from(menuItems).forEach((item) => {
+                item.classList.remove('active');
+            });
+            clickedMenuItem.classList.add('active');
+            leftMenuBar.classList.remove('open');
+            darkBg.classList.remove('block');
+            locateButton.textContent = clickedMenuItem.textContent.slice(0, 2);
+        }
+        event.stopPropagation();
+    });
+
+    darkBg.addEventListener('click', (event) => {
+        leftMenuBar.classList.remove('open');
+        darkBg.classList.remove('block');
+    });
+    // ------------------end left navbar script(author:YU)----------------------
+    // ------------------start right navbar script(author:YU)----------------------
+    const rightMenuBar = document.getElementById('right-menubar');
+    const menuButton = document.getElementById('menu-item');
+    const rightMenuItems = rightMenuBar.getElementsByClassName('item');
+    menuButton.addEventListener('mouseover', () => {
+        rightMenuBar.classList.add('open');
+        darkBg.classList.add('block');
+    });
+
+    rightMenuBar.addEventListener('click', (event) => {
+        const clickedItem = event.target;
+        if (clickedItem.classList.contains('item')) {
+            Array.from(rightMenuItems).forEach((item) => {
+                item.classList.remove('active');
+            });
+            clickedItem.classList.add('active');
+            rightMenuBar.classList.remove('open');
+            darkBg.classList.remove('block');
+        }
+        event.stopPropagation();
+    });
+
+    darkBg.addEventListener('click', (event) => {
+        rightMenuBar.classList.remove('open');
+        darkBg.classList.remove('block');
+    })
+
+    // ------------------start right navbar script(author:YU)----------------------
+</script>
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -6,33 +6,224 @@
 @include('_includes.introVideo')
 
 @section('PAGE_CONTENT')
-    <div class="video-wrapper">
-        <video class="video-section" id="landing-video" muted loop playsinline>
-            <source src="{{ asset('Video/LandingHD.mp4') }}" type="video/mp4">
-        </video>
-        <video class="video-section-mobile" id="landing-video-mobile" muted loop playsinline>
-            <source src="{{ asset('Video/LandingVM.mp4') }}" type="video/mp4">
-        </video>
-        <img class="menu-bg" src="{{ asset('images/Image1H.png') }}" alt="">
-        <img class="menu-bg-mobile" src="{{ asset('images/Image1V.png') }}" alt="">
-        <img class="cancel-video d-none" src="{{ asset('images/Stop.svg') }}" onclick="stopVideo_landing()">
-        <div class="content body-section">
-            <div id="text-container">
-                <div class="landing-text text-center">Everyone thinks of changing the world,</br> here at OIS, we do!</div>
-                <div class="landing-text text-center">We don't predict the future,</br>we create it!</div>
-                <div class="landing-text text-center">Service is not a department,</br>it's our entire company.</div>
-            </div>
-            <div class="w-100 d-flex flex-column justify-content-center align-items-center">
-                <p class="continue-text text-center">GO TO DASHBOARD</p>
-                <a href="{{ route('dashboard.index') }}">
-                    <img class="go-to-menu" src="{{ asset('images/IconMENU.svg') }}" />
-                </a>
-                <p class="continue-text text-center">GO TO LOGIN</p>
-                <a href="{{ route('login') }}">
-                    <img class="go-to-login" src="{{ asset('images/IconLOGIN.svg') }}" />
-                </a>
+    <div class="main-bg">
+        <div class="video-wrapper">
+            <video class="video-section" id="landing-video" muted loop playsinline>
+                <source src="{{ asset('Video/LandingHD.mp4') }}" type="video/mp4">
+            </video>
+            <video class="video-section-mobile" id="landing-video-mobile" muted loop playsinline>
+                <source src="{{ asset('Video/LandingVM.mp4') }}" type="video/mp4">
+            </video>
+            <img class="menu-bg" src="{{ asset('images/Image1H.png') }}" alt="">
+            <img class="menu-bg-mobile" src="{{ asset('images/Image1V.png') }}" alt="">
+            <img class="cancel-video d-none" src="{{ asset('images/Stop.svg') }}" onclick="stopVideo_landing()">
+
+        </div>
+        <div class="landing-section">
+            <div class="row m-0 menu-body">
+                <div class="navItem" onclick="">
+                    <div class="navItemWrap" onclick="">
+                        <div
+                            class="back-unvisible navItemWrap-front d-flex flex-column justify-content-center align-items-center">
+                        </div>
+                        <a class="menu-item navItemWrap-front d-flex justify-content-center align-items-center"
+                            href="{{ route('introduction.index') }}">
+                            <div class="h-100">
+                                <div class="d-flex justify-content-center h-50 align-items-end">
+                                    <img src="{{ asset('images/Logo/OISIcon.svg') }}">
+                                </div>
+                                <p class="back-unvisible menu-title">ABOUT OIS</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="navItem" onclick="">
+                    <div class="navItemWrap" onclick="">
+                        <div
+                            class="back-unvisible navItemWrap-front d-flex flex-column justify-content-center align-items-center">
+                        </div>
+                        <a class="menu-item navItemWrap-front d-flex justify-content-center align-items-center"
+                            href="{{ route('login') }}">
+                            <div class="h-100">
+                                <div class="d-flex justify-content-center h-50 align-items-end">
+                                    <img src="{{ asset('images/Logo/OISIcon.svg') }}">
+                                </div>
+                                <p class="back-unvisible menu-title">PASSPORT</p>
+                                <p class="menu-detail">APPLICATION</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="navItem" onclick="">
+                    <div class="navItemWrap" onclick="">
+                        <div
+                            class="back-unvisible navItemWrap-front d-flex flex-column justify-content-center align-items-center">
+                        </div>
+                        <a class="menu-item navItemWrap-front d-flex justify-content-center align-items-center"
+                            href="{{ route('login') }}">
+                            <div class="h-100">
+                                <div class="d-flex justify-content-center h-50 align-items-end">
+                                    <img src="{{ asset('images/Logo/OISIcon.svg') }}">
+                                </div>
+                                <p class="back-unvisible menu-title">BVN</p>
+                                <p class="menu-detail">APPLICATION</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="navItem" onclick="">
+                    <div class="navItemWrap" onclick="">
+                        <div
+                            class="back-unvisible navItemWrap-front d-flex flex-column justify-content-center align-items-center">
+                        </div>
+                        <a class="menu-item navItemWrap-front d-flex justify-content-center align-items-center"
+                            href="{{ route('login') }}">
+                            <div class="h-100">
+                                <div class="d-flex justify-content-center h-50 align-items-end">
+                                    <img src="{{ asset('images/Logo/OISIcon.svg') }}">
+                                </div>
+                                <p class="back-unvisible menu-title">TRACK VISA</p>
+                                <p class="menu-detail">APPLICATION</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="navItem" onclick="">
+                    <div class="navItemWrap" onclick="">
+                        <div
+                            class="back-unvisible navItemWrap-front d-flex flex-column justify-content-center align-items-center">
+                        </div>
+                        <a class="menu-item navItemWrap-front d-flex justify-content-center align-items-center"
+                            href="{{ route('login') }}">
+                            <div class="h-100">
+                                <div class="d-flex justify-content-center h-50 align-items-end">
+                                    <img src="{{ asset('images/Logo/OISIcon.svg') }}">
+                                </div>
+                                <p class="back-unvisible menu-title">VISA</p>
+                                <p class="menu-detail">APPLICATION</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="navItem" onclick="">
+                    <div class="navItemWrap" onclick="">
+                        <div
+                            class="back-unvisible navItemWrap-front d-flex flex-column justify-content-center align-items-center">
+                        </div>
+                        <a class="menu-item navItemWrap-front d-flex justify-content-center align-items-center"
+                            href="{{ route('services.index') }}">
+                            <div class="h-100">
+                                <div class="d-flex justify-content-center h-50 align-items-end">
+                                    <img src="{{ asset('images/Logo/OISIcon.svg') }}">
+                                </div>
+                                <p class="back-unvisible menu-title">SERVICES</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="navItem" onclick="">
+                    <div class="navItemWrap" onclick="">
+                        <div
+                            class="back-unvisible navItemWrap-front d-flex flex-column justify-content-center align-items-center">
+                        </div>
+                        <a class="menu-item navItemWrap-front d-flex justify-content-center align-items-center"
+                            href="{{ route('innovation.index') }}">
+                            <div class="h-100">
+                                <div class="d-flex justify-content-center h-50 align-items-end">
+                                    <img src="{{ asset('images/Logo/OISIcon.svg') }}">
+                                </div>
+                                <p class="back-unvisible menu-title">INNOVATION</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="navItem" onclick="">
+                    <div class="navItemWrap" onclick="">
+                        <div
+                            class="back-unvisible navItemWrap-front d-flex flex-column justify-content-center align-items-center">
+                        </div>
+                        <a class="menu-item navItemWrap-front d-flex justify-content-center align-items-center"
+                            href="{{ route('security.index') }}">
+                            <div class="h-100">
+                                <div class="d-flex justify-content-center h-50 align-items-end">
+                                    <img src="{{ asset('images/Logo/OISIcon.svg') }}">
+                                </div>
+                                <p class="back-unvisible menu-title">SECURITY</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="navItem" onclick="">
+                    <div class="navItemWrap" onclick="">
+                        <div
+                            class="back-unvisible navItemWrap-front d-flex flex-column justify-content-center align-items-center">
+                        </div>
+                        <a class="menu-item navItemWrap-front d-flex justify-content-center align-items-center"
+                            href="{{ route('ehealth.index') }}">
+                            <div class="h-100">
+                                <div class="d-flex justify-content-center h-50 align-items-end">
+                                    <img src="{{ asset('images/Logo/OISIcon.svg') }}">
+                                </div>
+                                <p class="back-unvisible menu-title">E-HEALTH</p>
+                                <p class="menu-detail">PORTAL</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="navItem" onclick="">
+                    <div class="navItemWrap" onclick="">
+                        <div
+                            class="back-unvisible navItemWrap-front d-flex flex-column justify-content-center align-items-center">
+                        </div>
+                        <a class="menu-item navItemWrap-front d-flex justify-content-center align-items-center"
+                            href="{{ route('money.index') }}">
+                            <div class="h-100">
+                                <div class="d-flex justify-content-center h-50 align-items-end">
+                                    <img src="{{ asset('images/Logo/OISIcon.svg') }}">
+                                </div>
+                                <p class="back-unvisible menu-title">MONEY</p>
+                                <p class="menu-detail">PORTAL</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="navItem" onclick="">
+                    <div class="navItemWrap" onclick="">
+                        <div
+                            class="back-unvisible navItemWrap-front d-flex flex-column justify-content-center align-items-center">
+                        </div>
+                        <a class="menu-item navItemWrap-front d-flex justify-content-center align-items-center"
+                            href="{{ route('shopping.index') }}">
+                            <div class="h-100">
+                                <div class="d-flex justify-content-center h-50 align-items-end">
+                                    <img src="{{ asset('images/Logo/OISIcon.svg') }}">
+                                </div>
+                                <p class="back-unvisible menu-title">SHOPPING</p>
+                                <p class="menu-detail">PORTAL</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="navItem last-panel" onclick="">
+                    <div class="navItemWrap" onclick="">
+                        <div
+                            class="back-unvisible navItemWrap-front d-flex flex-column justify-content-center align-items-center">
+                        </div>
+                        <a class="menu-item navItemWrap-front d-flex justify-content-center align-items-center"
+                            href="{{ route('contact.index') }}">
+                            <div class="h-100">
+                                <div class="d-flex justify-content-center h-50 align-items-end">
+                                    <img src="{{ asset('images/Logo/OISIcon.svg') }}">
+                                </div>
+                                <p class="back-unvisible menu-title">CUSTOMER</p>
+                                <p class="menu-detail">SUPPORT</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
+
     </div>
 @endsection
 
