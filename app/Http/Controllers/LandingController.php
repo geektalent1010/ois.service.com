a2365e3e76088df3cb72fd75ac1cf0893d986aa6
Full Stack Engineer <geektalent1010@gmail.com>
2024-02-08 19:32:34 -0800

    ADD: password protect page

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -9,4 +9,18 @@ class LandingController extends Controller
     public function landing() {
         return view ('landing');
     }
+
+    public function passwordProtected() {
+        return view('landing-password');
+    }
+
+    public function password(Request $req) {
+        $password = $req->password;
+        if($password == 'abcd123!@#') {
+            $req->session()->put('password', true);
+            return redirect()->route('landing');
+        } else {
+            return redirect()->back();
+        }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,77 +12,84 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/password-protected', 'LandingController@passwordProtected')->name('password.protected');
+Route::post('/password', 'LandingController@password')->name('password');
 
-Route::group([], function () {
-    Route::get('/', 'LandingController@landing')->name('home');
-    Route::get('/landing', 'LandingController@landing')->name('landing');
+
+Route::group(['middleware' => ['password.protected']], function () {
+
+    Auth::routes();
+
+    Route::group([], function () {
+        Route::get('/', 'LandingController@landing')->name('home');
+        Route::get('/landing', 'LandingController@landing')->name('landing');
+    });
+
+    // Auth Panel
+    Route::group(['middleware' => ['auth']], function () {
+
+        Route::get('/profile', 'ProfileController@index')->name('profile.index');
+        Route::post('/profile/update', 'ProfileController@updateProfile')->name('profile.update');
+
+        Route::get('/calendar', 'CalendarController@index')->name('calendar.index');
+
+        Route::get('/files', 'FilesController@index')->name('files.index');
+
+        Route::get('/news', 'NewsController@index')->name('news.index');
+
+        Route::get('/booking', 'BookingController@index')->name('booking.index');
+
+        Route::get('/iframe-booking', 'BookingController@iframeBooking')->name('booking.iframe');
+
+        Route::get('/admin', 'BookingController@iframeAdmin')->name('booking.iframe.admin');
+
+        Route::get('/application', 'ApplicationController@index')->name('application.index');
+
+        Route::get('/bvn-application', 'ApplicationController@bvn')->name('application.bvn');
+
+        Route::get('/checklists', 'ApplicationController@checklists')->name('checklists.index');
+        Route::post('/checklists/filter', 'ApplicationController@checklistFilter')->name('checklists.filter');
+    });
+
+
+
+    Route::get('/dashboard', 'DashboardController@index')->name('dashboard.index');
+
+    Route::get('/introduction', 'IntroductionController@index')->name('introduction.index');
+
+    Route::get('/services', 'ServicesController@index')->name('services.index');
+
+    Route::get('/innovation', 'InnovationController@index')->name('innovation.index');
+
+    Route::get('/security', 'SecurityController@index')->name('security.index');
+
+    Route::get('/ehealth', 'EhealthController@index')->name('ehealth.index');
+
+    Route::get('/money', 'MoneyController@index')->name('money.index');
+
+    Route::get('/shopping', 'ShoppingController@index')->name('shopping.index');
+
+    Route::get('/reach', 'ReachController@index')->name('reach.index');
+
+    Route::get('/support', 'ContactController@index')->name('contact.index');
+
+    Route::get('/centers', 'OfficesController@index')->name('offices.index');
+
+    Route::get('/terms', 'TermsController@index')->name('terms.index');
+
+    Route::get('/policy', 'PolicyController@index')->name('policy.index');
+
+    Route::get('/protection', 'ProtectionController@index')->name('protection.index');
+
+    Route::get('/solutions', 'SolutionsController@index')->name('solutions.index');
+    Route::get('/faq', 'FaqController@index')->name('faq.index');
+    Route::get('/book', 'BookController@index')->name('book.index');
+    Route::get('/contact/reviewing', 'ContactController@reviewing')->name('contact.reviewing');
+    Route::get('/backoffice', 'BackOfficeController@index')->name('backoffice.index');
+    Route::post('/offices/office-filter', 'OfficesController@officeFilter')->name('offices.search');
+
+
+
+    Route::get('/home', 'HomeController@index')->name('home');
+
 });
-
-
-// Auth Panel
-Route::group(['middleware' => ['auth']], function () {
-
-    Route::get('/profile', 'ProfileController@index')->name('profile.index');
-    Route::post('/profile/update', 'ProfileController@updateProfile')->name('profile.update');
-
-    Route::get('/calendar', 'CalendarController@index')->name('calendar.index');
-
-    Route::get('/files', 'FilesController@index')->name('files.index');
-
-    Route::get('/news', 'NewsController@index')->name('news.index');
-
-    Route::get('/booking', 'BookingController@index')->name('booking.index');
-
-    Route::get('/iframe-booking', 'BookingController@iframeBooking')->name('booking.iframe');
-
-    Route::get('/admin', 'BookingController@iframeAdmin')->name('booking.iframe.admin');
-
-    Route::get('/application', 'ApplicationController@index')->name('application.index');
-
-    Route::get('/bvn-application', 'ApplicationController@bvn')->name('application.bvn');
-
-    Route::get('/checklists', 'ApplicationController@checklists')->name('checklists.index');
-    Route::post('/checklists/filter', 'ApplicationController@checklistFilter')->name('checklists.filter');
-});
-
-
-Auth::routes();
-
-Route::get('/dashboard', 'DashboardController@index')->name('dashboard.index');
-
-Route::get('/introduction', 'IntroductionController@index')->name('introduction.index');
-
-Route::get('/services', 'ServicesController@index')->name('services.index');
-
-Route::get('/innovation', 'InnovationController@index')->name('innovation.index');
-
-Route::get('/security', 'SecurityController@index')->name('security.index');
-
-Route::get('/ehealth', 'EhealthController@index')->name('ehealth.index');
-
-Route::get('/money', 'MoneyController@index')->name('money.index');
-
-Route::get('/shopping', 'ShoppingController@index')->name('shopping.index');
-
-Route::get('/reach', 'ReachController@index')->name('reach.index');
-
-Route::get('/support', 'ContactController@index')->name('contact.index');
-
-Route::get('/centers', 'OfficesController@index')->name('offices.index');
-
-Route::get('/terms', 'TermsController@index')->name('terms.index');
-
-Route::get('/policy', 'PolicyController@index')->name('policy.index');
-
-Route::get('/protection', 'ProtectionController@index')->name('protection.index');
-
-Route::get('/solutions', 'SolutionsController@index')->name('solutions.index');
-Route::get('/faq', 'FaqController@index')->name('faq.index');
-Route::get('/book', 'BookController@index')->name('book.index');
-Route::get('/contact/reviewing', 'ContactController@reviewing')->name('contact.reviewing');
-Route::get('/backoffice', 'BackOfficeController@index')->name('backoffice.index');
-Route::post('/offices/office-filter', 'OfficesController@officeFilter')->name('offices.search');
-
-
-
-Route::get('/home', 'HomeController@index')->name('home');
