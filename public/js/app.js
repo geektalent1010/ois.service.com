67ffbbf91bf599ca842e27b61dad410f463bba20
Full Stack Engineer <geektalent1010@gmail.com>
2024-07-11 23:44:00 +0800

    UPDATE: scroll feature on the front page

--- public/js/app.js
+++ public/js/app.js
@@ -49555,8 +49555,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\Brandfields\project\ois.brandfields.com\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\Brandfields\project\ois.brandfields.com\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! E:\dev\Daniel\project\ois.brandfields.com\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! E:\dev\Daniel\project\ois.brandfields.com\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
