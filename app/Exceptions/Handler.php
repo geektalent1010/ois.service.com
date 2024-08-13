0f93e7433f066fc418dbbaf46f040e5533d79f55
Full Stack Engineer <geektalent1010@gmail.com>
2024-08-13 14:50:37 +0900

    FIX: 419 error page

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,6 +50,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if($exception instanceof \Illuminate\Session\TokenMismatchException) {
+            return redirect()->route('login')->with('error', 'Session expired. Please log in again.');
+        }
         return parent::render($request, $exception);
     }
 }
