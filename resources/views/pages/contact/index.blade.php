3bfd908bd83b4d454d8a69cd4f741895bb3ef4e5
Full Stack Engineer <geektalent1010@gmail.com>
2024-08-23 20:48:15 +0900

    FIX: support page issue

--- resources/views/pages/contact/index.blade.php
+++ resources/views/pages/contact/index.blade.php
@@ -126,7 +126,7 @@
                     if(res) {
                         customAlert("Success", "Your support message has been successfully sent! Our team will review your inquiry and get back to you as soon as possible", "success");
                         $("#name").val("");
-                        $("email").val("")
+                        $("#email").val("")
                         $("#message").val("")
                     }
                 }
